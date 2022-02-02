3c51e98e222484e3364cef306cccb3d341105418
Tarek <tarekhassouni5@gmail.com>
2022-02-02 10:21:51 +0100

    c6

--- src/main/java/cigma/pfe/MonApplication.java
+++ src/main/java/cigma/pfe/MonApplication.java
@@ -1,14 +1,16 @@
+package cigma.pfe;
+
+import cigma.pfe.controllers.ClientController;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import cigma.pfe.constrollers.ClientController;
 import cigma.pfe.models.Client;
 
-public class TestClient {
+public class MonApplication {
 
-	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-		ClientController ctrl = (ClientController) context.getBean("controller");
-		Client c = new Client(1L, "saad");
-		ctrl.save(c);
-	}
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        ClientController ctrl = (ClientController) context.getBean("controller");
+        Client c = new Client(1L, "saad");
+        ctrl.save(c);
+    }
 }
